240c6e629121f601737e5dd7548b2147fe482af5
Dinsert <korolkov.01@bk.ru>
2025-12-22 20:51:09 +0300

    - added cache Redis

--- src/main/java/com/example/user/profile/service/service/impl/UserProfileServiceImpl.java
+++ src/main/java/com/example/user/profile/service/service/impl/UserProfileServiceImpl.java
@@ -8,6 +8,8 @@ import com.example.user.profile.service.service.UserProfileService;
 import com.example.userprofile.api.dto.UserProfileDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,9 +24,11 @@ public class UserProfileServiceImpl implements UserProfileService {
     private final UserProfileMapper userProfileMapper;
 
 
+    @Cacheable(value = "user_profiles", key = "#userId")
     @Transactional(readOnly = true)
     @Override
     public UserProfileDto getUserProfile(UUID userId) {
+        log.info("DB call for get user profile {}", userId);
         UserProfile userProfile = userProfileRepository.findById(userId).orElseThrow(() -> {
             log.warn("User profile not found:{}", userId);
             return new UserProfileNotFoundException("User profile not found:" + userId);
@@ -32,9 +36,11 @@ public class UserProfileServiceImpl implements UserProfileService {
         return userProfileMapper.toDto(userProfile);
     }
 
+    @CacheEvict(value = "user_profiles", key = "#userId")
     @Transactional
     @Override
     public void upsertUserProfile(UUID userId, UserProfileDto dto) {
+        log.info("DB call for upsert user profile {}", userId);
         UserProfile profile = userProfileMapper.toEntity(dto);
         profile.setUserId(userId);
         userProfileRepository.save(profile);
